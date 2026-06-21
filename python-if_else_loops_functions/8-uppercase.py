be131008bf9fab43b312e43826caf243f0354fb5
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-06-21 06:22:58 -0700

    To uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@ def uppercase(str):
     """Print a string in uppercase."""
     for c in str:
         print(
-                "{:c}".format(
-                    ord(c) - 32 if ord(c) >= 97 and ord(c) <= 122 else ord(c)
-                    )
-                 ), end""
-        print()
+            "{:c}".format(
+                ord(c) - 32 if ord(c) >= 97 and ord(c) <= 122 else ord(c)
+            ), end=""
+        )
+    print()
