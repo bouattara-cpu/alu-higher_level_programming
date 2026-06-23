7ba13f8df76582e5fae59baa5800ba24247fe1e3
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-06-23 05:13:01 -0700

    Who are yo

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that prints the names defined in hidden_4.pyc, sorted alphabetically."""
+"""Module that prints the names defined in hidden_4.pyc"""
 hidden_4 = __import__("hidden_4")
 
 
