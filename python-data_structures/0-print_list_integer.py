ff8e6768ba4951e8c1519522f2915e41c16ae749
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-06-28 05:20:00 -0700

    Print a list of integers

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module that prints all integers of a list"""
+
+
 def print_list_integer(my_list=[]):
     """Print all integers of a list, one per line."""
     for number in my_list:
