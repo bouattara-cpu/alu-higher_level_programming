efb4070af68a24e740344bbe43a32ae06665df40
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-06-30 05:17:44 -0700

    fix

--- python-data_structures/3-main.py
+++ python-data_structures/3-main.py
@@ -10,6 +10,7 @@ def print_reversed_list_integer(my_list=[]):
     """
     if my_list is None:
         return
+
     length = len(my_list)
     for i in range(length - 1, -1, -1):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
