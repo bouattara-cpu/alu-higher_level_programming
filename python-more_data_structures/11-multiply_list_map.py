b3efb7535545e267fdd0b235504974b20dac6522
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-07-03 05:11:10 -0700

    Multiply by using map

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-"""Module that defines a function to multiply list values using map."""
-
-
-def multiply_list_map(my_list=[], number=0):
-    """Return a new list with values multiplied by number, using map."""
-    return list(map(lambda x: x * number, my_list))
+def multiply_list_map(lst=[],number=0):return list(map(lambda x:x*number,lst))
