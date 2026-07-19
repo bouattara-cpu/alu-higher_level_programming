6c3ddd3c4f54876e79c85327f2b6ea6b6258a516
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-07-19 11:31:14 -0700

    fix 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,7 +3,11 @@
 
 
 class Mylist(list):
-    """Class that inherits from list."""
+    """Represent a list.
+
+    This class inherits from the built-in list class and adds
+    a method to print the list in sorted order.
+    """
 
     def print_sorted(self):
         """Print the list, sorted in ascending order."""
