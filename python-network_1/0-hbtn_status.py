9bdf38decb27b6148b587032da04a828857ffce8
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-07-22 12:27:00 -0700

    Fix 0-hbtn_status.py with official format

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""
-Fetches https://intranet.hbtn.io/status.
-"""
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
-        print(f"\t- type: {type(body)}")
-        print(f"\t- content: {body}")
-        print(f"\t- utf8 content: {body.decode('utf-8')}")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
