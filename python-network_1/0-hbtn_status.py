02c27166dbb47bccc15165fb7732f8293a17ed06
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-07-22 12:18:44 -0700

    Add 0-hbtn_status.py

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches https://alu-intranet.hbtn.io/status using urllib."""
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alu-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
