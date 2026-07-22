82acdc26832d2e945db8d16457f7a4f58b1a6b27
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-07-22 12:51:11 -0700

    Fix 4-hbtn_status.py: restrict imports to requests only

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
+#!/usr/init/env python3
+# -*- coding: utf-8 -*-
 """
-Module that fetches a given URL using the requests package
-and displays response details.
+Module that fetches https://alu-intranet.hbtn.io/status
+using the requests package.
 """
-import sys
 import requests
 
 
 if __name__ == "__main__":
-    url = sys.argv[1] if len(sys.argv) > 1 else "https://alu-intranet.hbtn.io/status"
-    response = requests.get(url)
+    response = requests.get("https://alu-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
