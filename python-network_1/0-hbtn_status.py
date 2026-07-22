19b46c1ca492e994f22781e4031f860942da2efd
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-07-22 12:24:00 -0700

    Fix script to take URL argument and pass PEP8

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """
-Module that fetches https://alu-intranet.hbtn.io/status
+Module that fetches a given URL and displays response details.
 """
+import sys
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(body)}")
-        print(f"\t- content: {body}")
-        print(f"\t- utf8 content: {body.decode('utf-8')}")
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print("Body response:")
+            print(f"\t- type: {type(body)}")
+            print(f"\t- content: {body}")
+            print(f"\t- utf8 content: {body.decode('utf-8')}")
