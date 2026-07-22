e4d34d22a4846ffe872b633766f360fbdb05b9c2
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-07-22 13:26:03 -0700

    fix the file

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
 
-    response = requests.get("https://api.github.com/user",
-                             auth=(username, password))
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, password))
     print(response.json().get("id"))
