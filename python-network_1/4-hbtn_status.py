cc829839a44bdc3a26b8ffc06a4c87ce79d46357
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-07-22 12:55:02 -0700

    Support dynamic URL via sys.argv for task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """
-Module that fetches https://alu-intranet.hbtn.io/status
+Module that fetches https://alu-intranet.hbtn.io/status or a given URL
+using the requests package.
 """
+import sys
 import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
+    url = "https://alu-intranet.hbtn.io/status"
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+    response = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
