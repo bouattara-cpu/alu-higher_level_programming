9efe699d8e014a80db597e3a30747d751dd7bfee
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-07-22 12:48:56 -0700

    Fix 4-hbtn_status.py to handle dynamic URL arguments with requests

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
-Module that fetches https://alu-intranet.hbtn.io/status
-using the requests package.
+Module that fetches a given URL using the requests package
+and displays response details.
 """
+import sys
 import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
+    url = sys.argv[1] if len(sys.argv) > 1 else "https://alu-intranet.hbtn.io/status"
+    response = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
